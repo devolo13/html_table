561d2dcd9ef9041e30a5c271ac5ee557cd1366fb
devolo13 <devolo13@gmail.com>
2023-04-05 17:14:27 -0500

    initial commit

---
+++ test.py
@@ -0,0 +1,10 @@
+users = [
+        {'first_name': 'Michael', 'last_name': 'Choi'},
+        {'first_name': 'John', 'last_name': 'Supsupin'},
+        {'first_name': 'Mark', 'last_name': 'Guillen'},
+        {'first_name': 'KB', 'last_name': 'Tonel'}
+    ]
+
+for i in range(len(users)):
+    print(i)
+    print(users[i]['first_name'])
